a513616063a273a3902f4d1eb45d8d4dfe4c8e23
NeboCode <nebojsa.j@hotmail.rs>
2019-02-07 14:03:29 +0100

    Code refactor, improvements.

--- JS Game/app.js
+++ JS Game/app.js
@@ -10,20 +10,14 @@ GAME RULES:
 */
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
+init();
 
 // document.querySelector('#score-0').textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>'+ dice +'</em>';
 // var x = document.querySelector('#score-0').textContent;
 // document.querySelector('.dice').style.display = 'none';
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
 
 document.querySelector('.btn-roll').addEventListener('click',function (){
 
@@ -41,12 +35,60 @@ document.querySelector('.btn-roll').addEventListener('click',function (){
                 roundScore += dice;
                 document.querySelector('#current-' + activePlayer).textContent = roundScore;
         } else {
-                       activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-                       roundScore = 0;
 
-                       document.getElementById('current-0').textContent = '0';
-                       document.getElementById('current-1').textContent = '0';
-                       document.querySelector('.player-0-panel').classList.toggle('active');
-                       document.querySelector('.player-1-panel').classList.toggle('active');
+                nextPlayer();
+
         }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+        //Add current score to global score.
+        scores[activePlayer] += roundScore;
+        
+        // scores[activePlayer] += scores[activePlayer] + roundScore;
+
+        // Update UI
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+        // Check if player won game
+        if(scores[activePlayer] >= 20) {
+                document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+                document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+                document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+        } else {
+                       // Next player
+        nextPlayer(); 
+        }
+
+});
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function nextPlayer() {
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+function init() {
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-0-panel').classList.add('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+}
