af3582579ebcd303246c7e8cf674447041848ab6
NeboCode <nebojsa.j@hotmail.rs>
2019-02-09 10:53:23 +0100

    modified:   app.js 	modified:   index.html 	modified:   style.css

--- JS Game/app.js
+++ JS Game/app.js
@@ -23,20 +23,25 @@ document.querySelector('.btn-roll').addEventListener('click',function (){
 
         // 1. Random number
         if (gamePlaying) {
-                // var dice = Math.floor(Math.random()*6)+1;
-                var dice = 5;
+                var dice = Math.floor(Math.random()*6)+1;
+                var dice2 = Math.floor(Math.random()*6)+1;
+
 
                 // 2. Display the result
                 var diceDOM = document.querySelector('.dice');
                 diceDOM.style.display = 'block';
                 diceDOM.src = 'dice-' + dice + '.png';
         
+                var diceDOM2 = document.querySelector('.dice2');
+                diceDOM2.style.display = 'block';
+                diceDOM2.src = 'dice-' + dice2 + '.png';
+
                 // 3. Update the round score IF the rolled number was NOT a 1
-                if (dice !== 1) {
+                if ((dice !== 1) && (dice2 !== 1)) {
                         // Add score
                         if (sixRoll !== 1) {
                         console.log(sixRoll);
-                        roundScore += dice;
+                        roundScore += dice + dice2;
                         document.querySelector('#current-' + activePlayer).textContent = roundScore;
                 }
                         if (sixRoll !== 1) {
@@ -74,6 +79,7 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
                 gamePlaying = false;
                 document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
                 document.querySelector('.dice').style.display = 'none';
+                document.querySelector('.dice2').style.display = 'none';
                 document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
                 document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
