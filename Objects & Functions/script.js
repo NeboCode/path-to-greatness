424d00276cf3970f1d9dff1e94776529d28f9ca6
NeboCode <nebojsa.j@hotmail.rs>
2019-02-10 23:35:15 +0100

    modified:   script.js

--- Objects & Functions/script.js
+++ Objects & Functions/script.js
@@ -1,29 +1,127 @@
 // Function constructor
 
-var john = {
-    name: 'John',
-    yearOfBirth: 1990,
-    job: 'teacher'
-};
+// var john = {
+//     name: 'John',
+//     yearOfBirth: 1990,
+//     job: 'teacher'
+// };
 
-var Person = function(name, yearOfBirth, job) {
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
+// var Person = function(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
 
+// }
+
+// var john = new Person('John', 1990, 'teacher');
+// var jane = new Person('Jane', 1994, 'waitress');
+
+
+
+// Person.prototype.calculateAge = 
+// function() {
+//     console.log(2019 - this.yearOfBirth);
+// };
+
+// Person.prototype.lastName = 'Smith';
+
+// console.log(john.lastName);
+// jane.lastName;
+
+// Object.create
+
+
+// var personProto = {
+//     calculateAge: function() {
+//         console.log(2019 - this.yearOfBirth);
+//     }
+// };
+// var john = Object.create(personProto);
+
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = 'Teacher';
+
+
+// var jane = Object.create(personProto, {
+//     name: { value: 'Jane'},
+//     yearOfBirth: {value: 1995},
+//     job: {value: 'Online Tutor'}
+// });
+
+
+// Primites Vs Objects
+
+// var a = 23;
+// var b = a;
+// a = 46;
+
+// console.log(a);
+// console.log(b);
+
+// Now objects?
+
+
+
+// var obj1 = {
+//     name: 'Jaohn',
+//     age: 26
+// };
+
+// var obj2 = obj1;
+
+// obj1.age = 30;
+
+// console.log(obj1.age);
+// console.log(obj2.age);
+
+
+// var age = 27;
+// var obj = {
+//     name: 'Shone',
+//     city: 'Whatever'
+// };
+
+// function change(a,b) {
+//     a = 30;
+//     b.city = 'Anything';
+// };
+
+// change(age,obj);
+
+// console.log(age);
+// console.log(obj.city);
+
+
+// FUNCTIONS - Passing functions as arguments
+
+var years = [2007, 1991, 1994, 1995, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
 }
 
-var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1994, 'waitress');
 
+function calculateAge(el) {
+    return 2019 - el;
+}
+function isFullAge(el) {
+    return el >= 18;
+}
+function maxHeartRate(el) {
+    if(el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+    
+}
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages,isFullAge);
+var maxHeart = arrayCalc(ages,maxHeartRate);
 
-
-Person.prototype.calculateAge = 
-function() {
-    console.log(2019 - this.yearOfBirth);
-};
-
-Person.prototype.lastName = 'Smith';
-
-console.log(john.lastName);
-jane.lastName;
+console.log(maxHeart);
