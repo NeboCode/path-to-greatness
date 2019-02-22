033a1b33cc17db75835c94b8909554143b854ecd
NeboCode <nebojsa.j@hotmail.rs>
2019-02-22 10:47:08 +0100

    Add input logging

--- Budget App Project/app.js
+++ Budget App Project/app.js
@@ -15,16 +15,38 @@ var budgetController = (
 // UI Controller
 var UIController = (function() {
     // Some code later
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+
+        },
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 })();
+
 
 
 
 
 var controller = (function(budgetCtrl,UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
     var ctrlAddItem = function() {
         // 1. Get input data
-
+        var input = UICtrl.getinput();
+        console.log(input);
         // 2. Add item to the budget controller
 
         // 3. Add the item to the UI
@@ -33,7 +55,7 @@ var controller = (function(budgetCtrl,UICtrl) {
 
        // 5. Display budget on UI
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
         if (event.keyCode === 13 || event.which === 13) {
