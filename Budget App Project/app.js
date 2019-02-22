e5597d3bb020d12d26075b1726e4cc35636a8f47
NeboCode <nebojsa.j@hotmail.rs>
2019-02-22 10:27:35 +0100

    Added event listeners.

--- Budget App Project/app.js
+++ Budget App Project/app.js
@@ -2,33 +2,42 @@
 // Basically, they are closures inside an IIFE (Immediately Invoked Function Expression) returning an object which stores a function to give access to the inside scope.
 // Closures are functions that are using a function that has already returned a value.
 
-
+// Budget Controller
 var budgetController = (
     function() {
-        var x = 23;
-        var add = function(a) {
-            return x + a;
-        }
-        return {
-            publicTest: function(b) {
-                return add(b);
-            }
-        }
-    }
-)();
+    
+    
+    })();
 
+
+
+
+// UI Controller
 var UIController = (function() {
     // Some code later
-}
+})();
 
-)();
+
 
 
 var controller = (function(budgetCtrl,UICtrl) {
-    var z = budgetCtrl.publicTest(5);
-    return {
-        anotherPublicMethod: function() {
-            console.log(z);
-        }
+
+    var ctrlAddItem = function() {
+        // 1. Get input data
+
+        // 2. Add item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate budget
+
+       // 5. Display budget on UI
     }
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 })(budgetController,UIController);
